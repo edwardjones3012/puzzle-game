bc7abce99e22d06569c5da11fd0aead8a6c08036
edwardjones3012 <edwardjones3012@gmail.com>
2022-08-29 13:25:45 +0100

    Added grid scripts

---
+++ Assets/_Scripts/Grid/Grid.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Grid<T> {
+    private int width;
+    private int height;
+    private int cellSize;
+    private T[,] gridArray;
+    public Grid(int width, int height, int cellSize)
+    {
+        this.width = width;
+        this.height = height;
+        this.cellSize = cellSize;
+
+        gridArray = new T[width, height];
+
+        for (int x = 0; x < gridArray.GetLength(0); x++)
+        {
+            for (int y = 0; y < gridArray.GetLength(1); y++)
+            {
+                Debug.Log(x + ", " + y + GetWorldPosition(x,y));
+                Debug.DrawLine(GetWorldPosition(x, y), GetWorldPosition(x, y + 1), Color.cyan, 100000);
+                Debug.DrawLine(GetWorldPosition(x, y), GetWorldPosition(x + 1, y), Color.cyan, 100000);
+            }
+        }
+        Debug.DrawLine(GetWorldPosition(0, height), GetWorldPosition(width, height), Color.cyan, 100000);
+        Debug.DrawLine(GetWorldPosition(width, 0), GetWorldPosition(width, height), Color.cyan, 100000);
+    }
+
+    private Vector3 GetWorldPosition(int x, int y)
+    {
+        return new Vector3(x, 0, y) * cellSize;
+    }
+
+    public void SetValue(int x, int y, T value)
+    {
+        if (x >= 0 && y >= 0 && x < width && y < height)
+        {
+            gridArray[x, y] = value;
+            Debug.Log(gridArray[x, y]);
+        }
+    }
+}
---
+++ Assets/_Scripts/Grid/GridTESTING.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GridTESTING : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Grid<int> grid = new Grid<int>(5, 5, 2);
+    }
+}
